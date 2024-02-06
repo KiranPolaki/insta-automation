a198b5317141a64270021c0f5e363a96f0a0d635
polakikiran <polakisaikiran2@gmail.com>
2024-02-06 16:56:33 +0530

    added server

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express"),
+  { urlencoded, json } = require("body-parser"),
+  bodyParser = require("body-parser"),
+  crypto = require("crypto"),
+  path = require("path"),
+  app = express().use(bodyParser.json());
+
+app.get("/api", function (req, res) {
+  console.log("Get called");
+  console.log(req.body);
+
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === "helloworld") {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.post("/api", function (req, res) {
+  console.log("Post called");
+  console.log(JSON.stringify(req.body));
+  const data = JSON.stringify(req.body);
+
+  console.dir(req.body);
+
+  if (req.body.object == "comments") {
+    console.log("In comments");
+  }
+
+  console.log(data);
+
+  res.status(200).send("EVENT_RECEIVED");
+});
+
+const port = process.env.PORT || 5500;
+app.listen(port, () => console.log(`Listening on Port: ${port}`));
